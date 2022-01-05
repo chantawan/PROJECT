e257b220dd3fdfea03b4e03fd79a9461d4c1b34d
Thadphong <97097473+Thadphong@users.noreply.github.com>
2022-01-05 18:46:12 +0700

    Add files via upload

---
+++ view_booking_now.php
@@ -0,0 +1,36 @@
+<?php
+	include 'connect.php';
+	
+    $date_today = $_POST["date_today"];
+
+	$sql_query = "SELECT  m_firstname , m_lastname , stadium_name , booking_date , time_start , time_end , all_time , total 
+    FROM booking a, member b, stadium c
+    WHERE a.stadium_id = c.stadium_id and b.m_id = a.m_id and a.booking_date = '$date_today' ORDER BY 'a'.'time_start' ASC";
+
+    $result = mysqli_query($conn,$sql_query);
+    $num_row = mysqli_num_rows($result);
+
+    if ($num_row > 0) {
+        while($row = $result->fetch_assoc()) {
+    ?>	
+
+        <tr>
+            <td><?=$row['m_firstname'];?></td>
+            <td><?=$row['m_lastname'];?></td>
+            <td><?=$row['stadium_name'];?></td>
+            <td><?=$row['booking_date'];?></td>
+            <td><?=$row['time_start'];?></td>
+            <td><?=$row['time_end'];?></td>
+            <td><?=$row['all_time'];?></td>
+            <td><?=$row['total'];?></td>
+        </tr>
+
+    <?php	
+        }
+    }
+    else {
+        echo "ไม่พบผลลัพธ์";
+    }
+	mysqli_close($conn);
+?>
+  
